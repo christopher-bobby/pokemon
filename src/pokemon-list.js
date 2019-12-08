dfdccfd32577ac4f4e5e6ee270bdb0796c5be935
Christopher <christoph0895@gmail.com>
2019-12-08 22:06:38 +0700

    add pokemon list

--- src/pokemon-list.js
+++ src/pokemon-list.js
@@ -1,18 +1,31 @@
-import React, { Component, useEffect, useState, useContext } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import { Link, NavLink } from "react-router-dom";
+
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 
-
-function PokemonList () {  
-
-
-
-  
+ function PokemonList () {  
+  const [pokemonList, setPokemonList] = useState([]);
+  const getData = () => {
+    axios.get('https://pokeapi.co/api/v2/pokemon/').then((response) =>
+      //console.log("res", response.data);
+      setPokemonList(response.data.results)
+    )
+  }
+  useEffect(()=>{
+    getData();
+  }, [])
   return (
-    <div className="pokemon-list-container">
-    
+    <div className="pokemon-list">
+
+      {
+        pokemonList.length !== 0 && pokemonList.map((pokemon, index) =>{
+          return (
+            <div><NavLink to={`/pokemon-detail/${index+1}`}>{pokemon.name}</NavLink></div>
+          )
+        })
+      }
     </div>
   );
   
